f7fc7c57912c3c15d396bee824bda0046cfce512
OlgaPopova <109219990+OlgaPopovaHappy@users.noreply.github.com>
2022-09-19 22:48:06 +0300

    Касание к С# №4 :)

---
+++ HW-004/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+if (number % 2 == 0)
+{
+    WriteLine($"{number} - четное число.");
+}
+else
+{
+    WriteLine($"{number} - нечетное число.");
+}
